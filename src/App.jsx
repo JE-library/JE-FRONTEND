544e302645116db1d87edd4ad5c11a0213051901
Josephine <jossyasiedu96@gmail.com>
2025-06-12 13:51:17 +0000

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Books from './pages/Books'
 import BookForm from './pages/BookForm'
 import { createBrowserRouter,RouterProvider } from 'react-router'
 import EditForm from './pages/EditForm'
+import BookDetails from './pages/BookDetails'
 
 
 
@@ -21,6 +22,10 @@ function App() {
   {
     path: "/add-book",
     element: <BookForm/>
+  },
+  {
+    path: "/book-details/:id",
+    element: <BookDetails/>
   },
   {
     path: "/edit-book",
