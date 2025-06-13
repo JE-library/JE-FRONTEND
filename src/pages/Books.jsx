f90177151efb13bf3f854d70481b8d0f9208c5b1
efbrace <ebenezerflintwoodbrace@gmail.com>
2025-06-13 14:45:43 +0000

    Update Books.jsx

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import PagesLayout from "../Layouts/PagesLayout";
+import PagesLayout from "../layouts/PagesLayout";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import logo from "../assets/LOGO.png";
